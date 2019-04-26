ce31b29aa9763876b7f40cb984b7b5dc81aed9e1
Michael Kluge <vollseil@arcor.de>
2019-04-26 17:33:48 +0000

    initial start of the aplcontrol code

---
+++ aplcontrol/aplcontrol/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for aplcontrol project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aplcontrol.settings')
+
+application = get_wsgi_application()
