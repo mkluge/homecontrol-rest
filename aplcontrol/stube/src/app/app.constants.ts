0e3d4684474dee423cff3c7587728d2780162f09
Michael Kluge <vollseil@gmail.com>
2020-11-18 20:09:45 +0100

    moving to Home Assistant, no need for own UI

--- aplcontrol/stube/src/app/app.constants.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class Configuration {
-    public MediaRestServer = 'http://192.168.178.38:5000/';
-    public SatUrl = 'sat/';
-    public SatApiUrl = this.MediaRestServer + this.SatUrl;
-}
