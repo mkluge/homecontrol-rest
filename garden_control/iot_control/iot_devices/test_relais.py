a83f1a1b48133d9917b869ca67d1bc7ae114b2b8
Michael Kluge <vollseil@gmail.com>
2020-12-29 17:06:27 +0100

    removed old file

--- garden_control/iot_control/iot_devices/test_relais.py
+++
@@ -1,7 +0,0 @@
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM) # GPIO Nummern statt Board Nummern
-GPIO.setwarnings(False)
- 
-RELAIS_1_GPIO = 17
-GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Modus zuweisen
-GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # aus
