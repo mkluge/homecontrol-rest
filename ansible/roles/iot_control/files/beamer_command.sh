737bd81c3283df36faed766e3e3acb11d4ad42f4
Michael Kluge <vollseil@gmail.com>
2021-01-03 22:44:20 +0100

    made ansible role for iot_control

---
+++ ansible/roles/iot_control/files/beamer_command.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
+/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
+/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
+/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
+/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
+/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
+/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
+/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
+/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
