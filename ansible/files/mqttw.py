9e178f1a359244d33be49c5693efd5d19c70809b
Michael Kluge <vollseil@gmail.com>
2021-01-16 22:03:10 +0100

    adding missing argument

--- ansible/files/mqttw.py
+++ ansible/files/mqttw.py
@@ -14,14 +14,14 @@ class MqttWrapper:
         self.connected_flag = False
         self.client.loop_start()
 
-    def on_connect(self, mqtt_client, _userdata, _flags, rc):
+    def on_connect(self, mqtt_client, userdata, flags, rc):
         if rc == 0:
             self.connected_flag = True
         else:
             self.connected_flag = False
         print("Connected with result code " + str(rc))
 
-    def on_disconnect(self, _mqtt_client, _userdata, rc):
+    def on_disconnect(self, mqtt_client, userdata, rc):
         print("disconnecting reason  " + str(rc))
         self.connected_flag = False
 
