bdda856a45fe9b12d77fbc837ffc2a81056dcefe
Michael Kluge <vollseil@gmail.com>
2021-03-01 09:44:58 +0100

    best way to send for the moment

--- ansible/roles/lircd_stube/files/beamer_command.sh
+++ ansible/roles/lircd_stube/files/beamer_command.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
-/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
-/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
-/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
-/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
-/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
-/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
-/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
-/usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1
+for I in `seq 1 50` ; do /usr/bin/irsend --device=/var/run/lirc/lircd-tx SEND_ONCE EPSON-TW6000 $1 ; done
